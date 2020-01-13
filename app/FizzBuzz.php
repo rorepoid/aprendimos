a6e1c5b47bfdfe4cb56d9c26b96ce1ebd4f9df31
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-01-12 22:53:27 -0500

    separate conditions in functions

--- app/FizzBuzz.php
+++ app/FizzBuzz.php
@@ -7,21 +7,33 @@ class FizzBuzz {
         $list = range(1, 20);
         $list = array_map(function($number){
             switch ($number) {
-                case $number % 5 == 0 && $number % 3 == 0: 
-                    return 'FizzBuzz';
-                    break;
-                case $number % 3 == 0:
-                    return 'Fizz';
-                    break;
-                case $number % 5 == 0: 
-                    return 'Buzz';
-                    break;
+                case $this->isFizzBuzzFor($number): 
+                    return 'FizzBuzz'; break;
+                case $this->isFizzFor($number):
+                    return 'Fizz'; break;
+                case $this->isBuzzFor($number): 
+                    return 'Buzz'; break;
                 default:
-                    return $number;
-                    break;
+                    return $number; break;
             }
         }, $list);
 
         return $list;
     }
+
+    public function isFizzFor($number)
+    {
+        return $number % 3 == 0;  
+    }
+    
+    public function isBuzzFor($number)
+    {
+        return $number % 5 == 0;  
+    }
+
+    public function isFizzBuzzFor($number)
+    {
+        return $number % 3 == 0 && $number % 5 == 0;
+    }
+
 }
