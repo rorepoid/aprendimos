2915b40365c21aeb52eac2c97177cd03d7425692
Roberto Orellana <ritorito.141516@gmail.com>
2020-02-15 18:47:35 -0500

    Add concept to Active Learning

--- app/Concepts/Types/ActiveLearning/ActiveLearning.php
+++ app/Concepts/Types/ActiveLearning/ActiveLearning.php
@@ -6,6 +6,14 @@ namespace App\Concepts\Types\ActiveLearning;
 
 use App\Concepts\Learning;
 
+/**
+ * Class ActiveLearning
+ * Active learning is a form of learning in which teaching strives to involve students
+ * in the learning process more directly than in other methods.
+ * In active learning, students participate in the process
+ * and students participate when they are doing something besides passively listening.
+ * @package App\Concepts\Types\ActiveLearning
+ */
 class ActiveLearning extends Learning
 {
 
