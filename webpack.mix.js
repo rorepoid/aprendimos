027abd1cee7546e6cbea4ce24012f9d2141ae07b
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-03-29 02:10:10 -0500

    add tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/nprogress.css', 'public/css/app.css')
+    .postCss('resources/css/app.css', 'public/css', [
+      require('tailwindcss'),
+    ])
     .sass('resources/sass/app.scss', 'public/css')
     .webpackConfig({
      output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
