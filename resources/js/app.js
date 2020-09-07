3848976d1da8d0eed50911adc4d1db07e79542c8
Roberto Orellana <rorepoid@gmail.com>
2020-09-06 19:50:35 -0500

    build: Replace vue to react (#10)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,13 @@
-import { InertiaApp } from '@inertiajs/inertia-vue'
-import Vue from 'vue'
-
-Vue.prototype.$route = (...args) => route(...args).url()
-Vue.use(InertiaApp)
+import { InertiaApp } from '@inertiajs/inertia-react'
+import React from 'react'
+import { render } from 'react-dom'
 
 const app = document.getElementById('app')
 
-new Vue({
-  render: h => h(InertiaApp, {
-    props: {
-      initialPage: JSON.parse(app.dataset.page),
-      resolveComponent: name => import(`./Pages/${name}`).then(module => module.default),
-    },
-  }),
-}).$mount(app)
+render(
+  <InertiaApp
+    initialPage={JSON.parse(app.dataset.page)}
+    resolveComponent={name => require(`./Pages/${name}`).default}
+  />,
+  app
+)
